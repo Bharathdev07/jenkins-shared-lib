fff5e4649df9153731955b48685d08f120dfcbca
Bharathdev07 <devops854@gmail.com>
2025-03-20 12:14:48 +0530

    Create gitcheckout.groovy

---
+++ vars/gitcheckout.groovy
@@ -0,0 +1,7 @@
+def call() {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: '*/master']],  
+        userRemoteConfigs: [[url: "https://github.com/Bharathdev07/springpetclinic--project.git"]]
+    ])
+}
